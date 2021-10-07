1a9557d9c3702125c4e727bc9f3849a00aaebaa5
wangmengdong <wangmengdong@bytedance.com>
2021-10-07 22:23:59 +0800

    feat: init

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'JUMP GAME',
+    }),
+  ],
+};
